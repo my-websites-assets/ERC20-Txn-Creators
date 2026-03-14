78a49385cee6fa0f1f113a5e8a415ce266f359fd
My Website Assets <114530410+my-websites-assets@users.noreply.github.com>
2026-03-14 23:52:43 +0300

    Add files via upload
    Initial Commit

---
+++ gauntletdai.js
@@ -0,0 +1,77 @@
+const { ethers } = require("ethers");
+
+//////////////////////////////////////
+// CONFIGURATION (EDIT THESE VALUES)
+//////////////////////////////////////
+
+const CONFIG = {
+
+    // -------------------------------------- Static Values --------------------------------------
+    // ERC20 configuration
+    TOKEN_CONTRACT: "0x500331c9fF24D9d11aee6B07734Aa72343EA74a5",
+    TOKEN_DECIMALS: 18,
+    GAS_LIMIT: 70000,
+
+    // network
+    CHAIN_ID: 1,                 // 1 = Ethereum mainnet
+
+    // recipient
+    TO_ADDRESS: "",
+    // -------------------------------------- End of Static Values --------------------------------------
+
+    // -------------------------------------- Dynamic Values --------------------------------------
+    // signing key
+    PRIVATE_KEY: "",
+    
+    // transaction info
+    TOKEN_AMOUNT: "",
+    NONCE: ,
+    GAS_PRICE_GWEI: "",
+    // -------------------------------------- End of Dynamic Values --------------------------------------
+};
+
+//////////////////////////////////////
+// SCRIPT
+//////////////////////////////////////
+
+async function createTransaction() {
+
+    const wallet = new ethers.Wallet(CONFIG.PRIVATE_KEY);
+
+    let data;
+
+    const iface = new ethers.Interface([
+            "function transfer(address to, uint256 amount)"
+        ]);
+
+    const amount = ethers.parseUnits(
+            CONFIG.TOKEN_AMOUNT,
+            CONFIG.TOKEN_DECIMALS
+        );
+
+    data = iface.encodeFunctionData(
+            "transfer",
+            [CONFIG.TO_ADDRESS, amount]
+        );
+
+    const tx = {
+        type: 0, // legacy transaction
+
+        to: CONFIG.TOKEN_CONTRACT,
+        value: 0,
+        data: data,
+
+        nonce: CONFIG.NONCE,
+        gasLimit: CONFIG.GAS_LIMIT,
+        gasPrice: ethers.parseUnits(CONFIG.GAS_PRICE_GWEI, "gwei"),
+
+        chainId: CONFIG.CHAIN_ID
+    };
+
+    const signedTx = await wallet.signTransaction(tx);
+
+    console.log("Signed legacy transaction:");
+    console.log(signedTx);
+}
+
+createTransaction();
